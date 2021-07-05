fd8c7d072450b28a481c4ac85cf0edbf92f71b78
sandipfachra <sandipfachara35@gmail.com>
2021-07-05 09:25:32 +0530

    node and expressjs project

---
+++ routes/main.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const controllers = require('../controllers')
+
+router.get('/', async (req, res, next) => {
+    const data = req.context
+
+    const itemCtr = controllers.item.instance()
+    data.coffee = await itemCtr.get({category: 'coffee'})
+    data.tea = await itemCtr.get({category: 'tea'})
+    data.pastries = await itemCtr.get({category: 'pastries'})
+
+    res.render('home', data)
+})
+
+
+router.get('/blog', (req, res, next) => {
+    res.render('blog', req.context)
+})
+
+router.get('/items', async (req, res, next) => {
+    const filter = req.query
+    const itemCtr = controllers.item.instance()
+    const items = await itemCtr.get(filter)
+
+    res.json({
+        items
+    })
+})
+
+router.post('/order', async (req, res, next) => {
+    const orderData = req.body
+
+    const orderCtr = controllers.order.instance()
+    const order = await orderCtr.post(orderData)
+
+    res.json(order)
+})
+
+
+module.exports = router
